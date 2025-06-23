ad9d037d3b0158f2dfad03bc7cddb19436e2edd0
Merten Stender <merten.stender@tu-berlin.de>
2025-06-23 21:38:38 +0200

    resolve issues with incorrect density and spectral radius
    Signed-off-by: Merten Stender <merten.stender@tu-berlin.de>

--- tests/test_reservoir_layer.py
+++ tests/test_reservoir_layer.py
@@ -4,22 +4,15 @@ Implements tests for the ReservoirLayer class.
 
 import pytest
 import numpy as np
-
 from pyreco.custom_models import RC as RC
-from pyreco.layers import InputLayer, ReadoutLayer
-from pyreco.layers import RandomReservoirLayer
-
+from pyreco.layers import InputLayer, ReadoutLayer, RandomReservoirLayer
 from pyreco.utils_networks import compute_density, compute_spec_rad
 
 
-
-
-"""
-Setting the reservoir weights (adjacency matrix, reservoir network) form the outside
-"""
-
-
 def test_set_weights():
+    """
+    Setting the reservoir weights (adjacency matrix, reservoir network) form the outside
+    """
 
     # create a simple RC model and then set the weights
     model = RC()
@@ -28,25 +21,23 @@ def test_set_weights():
     model.add(ReadoutLayer(output_shape=(1, 100)))
     model.compile()
 
+    # check compilation boolean flag
+    assert model.reservoir_layer._is_compiled is True
+
     # obtain current weights
     curr_weights = model.reservoir_layer.weights
     curr_density = compute_density(curr_weights)
     curr_spec_rad = compute_spec_rad(curr_weights)
 
     # now let pytest check that the current weights are correctly set in the layer
-    # print(f'Current density: {curr_density}, Current spectral radius: {curr_spec_rad}')
-    #print(f'Model reservoir layer density: {model.reservoir_layer.density}, Model spectral radius: {model.reservoir_layer.spec_rad}')
-    # TODO issue #76 (update those properties after the weights are set)
-    # assert np.isclose(curr_density, model.reservoir_layer.density)
-    # assert np.isclose(curr_spec_rad, model.reservoir_layer.spec_rad)
+    assert np.isclose(curr_density, model.reservoir_layer.density)
+    assert np.isclose(curr_spec_rad, model.reservoir_layer.spec_rad)
 
     # Create new weights with the same shape and set them
     new_weights = np.ones_like(curr_weights)
     model.reservoir_layer.set_weights(new_weights)
-
     new_density = compute_density(new_weights)
     new_spec_rad = compute_spec_rad(new_weights)
-    # print(f"New density: {new_density}, New spectral radius: {new_spec_rad}")
 
     # now let pytest check that the new weights are correctly updated in the layer
     assert np.array_equal(model.reservoir_layer.weights, new_weights)
